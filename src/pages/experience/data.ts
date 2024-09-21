a16843918ca1d0a46a21adbd86c580ab6bd71647
clinton otbana <rbnclntn@gmail.com>
2024-09-21 19:00:37 +0800

    link fixed

--- src/pages/experience/data.ts
+++ src/pages/experience/data.ts
@@ -23,7 +23,7 @@ const data: ExperiencesType[] = [
     description:
       "Collaborated with the development team to enhance and debug the company's administrative and business websites. Responsible for translating design specifications into code, adding new features and improve functionality.",
     tools: ["PHP", "MySQL", "JQuery", "JavaScript", "HTML", "CSS"],
-    url: "https://www.google.com/search?q=five+freedom+partners&rlz=1C1KNTJ_enPH1029PH1029&oq=five+f&gs_lcrp=EgZjaHJvbWUqCAgAEEUYJxg7MggIABBFGCcYOzIHCAEQLhiABDIGCAIQRRg5MgoIAxAuGLEDGIAEMgcIBBAuGIAEMgcIBRAAGIAEMgYIBhBFGDwyBggHEEUYPKgCALACAA&sourceid=chrome&ie=UTF-8",
+    url: "https://ffpi.one/",
   },
   {
     job_title: "Frontend Developer Intern",
@@ -32,7 +32,7 @@ const data: ExperiencesType[] = [
     description:
       "Successfully completed the 350-hour On-The-Job course as required by my university. I take part in building the UI of the company's 'Web Based User Management System' by translating a Figma design into code.",
     tools: ["Vue", "Quasar"],
-    url: "https://software.pixel8.ph",
+    url: "https://pixel8websolutions.com/",
   },
 ];
 
